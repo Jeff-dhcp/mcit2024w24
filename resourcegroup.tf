f70babadf0847845b6370ba2f731e23b28f96443
Jeff-dhcp <jeff.sidibe888@outlook.com>
2024-09-20 19:49:25 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "mcit420zz5um" {
   name     = "jeff_resource_group"
   location = "canadacentral"
 }
+
+resource "azurerm_resource_group" "mcitlllll5um" {
+  name     = "ada_resource_grouppp"
+  location = "eastus"
+}
