96f22c11f4786ea9b03c43b8c196b4e237283180
Jeff-dhcp <jeff.sidibe888@outlook.com>
2024-09-23 19:57:28 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,18 @@ output "nameofthevariable" {
 description="variable"
 value=var.cloud
 }
+
+output "nameofthelocation" {
+description="variable"
+value=var.southkorea
+}
+
+output "nameofthelocation2" {
+description="variable"
+value=var.ukwest
+}
+
+output"nameofthelocation3" {
+description="variable"
+value=var.uscentral
+}
