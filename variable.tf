fcae4bf99acc3eae317f811ead8d7f546219c34c
Jeff-dhcp <jeff.sidibe888@outlook.com>
2024-09-23 18:43:52 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -16,3 +16,27 @@ type=string
 default="myresourcegroup"
 
 }
+variable "loadbalancer" {
+type=string
+default="loadbalancer"
+}
+variable "OSPF" {
+type=string
+default="OSPF"
+}
+variable "network" {
+type=string
+default="network"
+}
+variable "subnet" {
+type=string
+default="subnet"
+}
+variable "secret" {
+type=string
+default="secret"
+}
+variable "microsoft" { 
+type=string
+default="microsoft"
+}
