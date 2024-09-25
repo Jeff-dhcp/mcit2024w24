f88fff862845567099c099333c4f62ea09d02910
Jeff-dhcp <jeff.sidibe888@outlook.com>
2024-09-25 18:41:50 -0400

    Update storagecontainer.tf

--- storagecontainer.tf
+++ storagecontainer.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "storagecontainer" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestoraccount"
+  name                     = "storagecontainer"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
