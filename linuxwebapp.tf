febc195b2244f1f2ae597f36fe74cf478b331f65
Jeff-dhcp <jeff.sidibe888@outlook.com>
2024-09-27 19:15:18 -0400

    Update linuxwebapp.tf

--- linuxwebapp.tf
+++ linuxwebapp.tf
@@ -24,3 +24,33 @@ resource "azurerm_linux_web_app" "example" {
 
   site_config {}
 }
+
+
+
+
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = var.service_plan_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = var.os_type
+  sku_name            = var.sku_name
+}
+
+resource "azurerm_linux_web_app" "example" {
+  name                = var.web_app_name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {}
+}
