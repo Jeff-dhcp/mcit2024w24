b7cf5d300327c647ba5ae74ba35741509a702d73
Jeff-dhcp <jeff.sidibe888@outlook.com>
2024-09-27 19:19:10 -0400

    Update linuxwebapp.tf

--- linuxwebapp.tf
+++ linuxwebapp.tf
@@ -1,7 +1,5 @@
 
-provider "azurerm" {
-  features {}
-}
+
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
@@ -16,7 +14,7 @@ resource "azurerm_service_plan" "example" {
   sku_name            = "P1v2"
 }
 
-resource "azurerm_linux_web_app" "example" {
+resource "azurerm_linux_web_app" "example2" {
   name                = "example"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
@@ -29,28 +27,4 @@ resource "azurerm_linux_web_app" "example" {
 
 
 
-provider "azurerm" {
-  features {}
-}
 
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_service_plan" "example" {
-  name                = var.service_plan_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  os_type             = var.os_type
-  sku_name            = var.sku_name
-}
-
-resource "azurerm_linux_web_app" "example" {
-  name                = var.web_app_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
-
-  site_config {}
-}
