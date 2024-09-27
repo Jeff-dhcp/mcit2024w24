68ee8e29138c6adfc38fd11e42ad4e0335bea86a
Jeff-dhcp <jeff.sidibe888@outlook.com>
2024-09-27 19:25:10 -0400

    Update linuxwebapp.tf

--- linuxwebapp.tf
+++ linuxwebapp.tf
@@ -9,12 +9,12 @@ resource "azurerm_resource_group" "example" {
 resource "azurerm_service_plan" "example" {
   name                = "example"
   resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  location            = azurerm_resource_group.example.centralus
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
 
-resource "azurerm_linux_web_app" "example2" {
+resource "azurerm_linux_web_app" "" {
   name                = "example"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
