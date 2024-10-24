57e5b06451e0ffd08599b86a4a5e33a97036cbc0
Jeff-dhcp <jeff.sidibe888@outlook.com>
2024-10-23 20:29:57 -0400

    Update mainexam.tf

--- mainexam.tf
+++ mainexam.tf
@@ -80,3 +80,13 @@ resource "null_resource" "car_brands" {
     command = "echo 'This is a ${each.key} car'"
   }
 }
+
+
+
+
+resource "azurerm_virtual_network" "example" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
