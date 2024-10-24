8464c0a1f11254218f31cb5d35544093e59b65f9
Jeff-dhcp <jeff.sidibe888@outlook.com>
2024-10-23 20:23:57 -0400

    Update mainexam.tf

--- mainexam.tf
+++ mainexam.tf
@@ -5,9 +5,7 @@
 resource "azurerm_virtual_network" "example" {
   name                = "mcitexam-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
+
 
 resource "azurerm_subnet" "example" {
   name                 = "mcitexam-subnet"
